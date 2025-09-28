967bd47328297c1892a7e2a0af046e5b4fc198cd
Miraç Kaan Şengül <mirac.kaansengull@gmail.com>
2025-09-28 17:55:02 +0300

    düzeltme

--- vite.config.js
+++ vite.config.js
@@ -7,6 +7,7 @@ export default defineConfig({
   base: process.env.NODE_ENV === 'production' ? '/doktor_web/' : '/',
   build: {
     outDir: 'dist',
-    assetsDir: 'assets'
+    assetsDir: 'assets',
+    copyPublicDir: true
   }
 })
